4ee8b14049975d69e6b0fe6f0b0b720f4c848415
Invertex3 <anselmixdmenonlaverne@gmail.com>
2025-12-07 12:10:38 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/app.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
